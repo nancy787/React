c10209d32a72c7d1f76492426f33b132200a7043
nancy@newspiritz.com <nancy@newspiritz.com>
2025-08-08 11:30:51 +0530

    Added composenets

---
+++ MegaBlog/src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Button( {
+    children,
+    type = 'button',
+    bgcolor = 'bg-blue-600',
+    textColor = '',
+    ClassName = '',
+    ...props
+}){
+    return (
+        <button className={`px-4 py-2 rounded-lg ${className} ${bgcolor} ${textColor}`} {...props}>
+            {children}
+        </button>
+    )
+}
+
+export default Button
--- MegaBlog/src/components/header/Header.jsx
+++ MegaBlog/src/components/header/Header.jsx
@@ -1,10 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import {Container, Logo, LogoutBtn} from "../index"
+import {Link, useNavigate} from "react-router-dom"
+import { UseSelector, useSelector } from "react-redux";
 
 function Header() {
+    const authStatus = useSelector((state) => state.auth.status)
+    const navigate = useNavigate()
+    const navItems  = [
+        {
+            'name' : 'Home',
+            'slug' : '/',
+            'active' : true
+        },{
+            'name' : 'Login', 
+            'slug' : '/login',
+            active : !authStatus,
+        },
+        {
+            'name' : 'Signup', 
+            'slug' : '/signup',
+            active : !authStatus,
+        },{
+            'name' : 'All Post',
+            'slug' : '/all-posts',
+            active : authStatus
+        },{
+            'name' : 'Add Post',
+            'slug' : 'add-posts',
+            active : authStatus
+        },{
+            'name' : 'Update Post',
+            'slug' : '/update-posts:{id}',
+             active : authStatus
+        }
+
+    ]
     return (
-        <div>
-        <h1>Header</h1>
-        </div>
+        <header className="py-3 shadow bg-gray-500">
+            <Container>
+                <nav className="flex">
+                    <div className="mr-4">
+                        <Link to='/'>
+                            <Logo width='70px'/>
+                        </Link>
+                    </div>
+                    <ul className="flex ml-auto">
+                        {navItems.map( (item) => 
+                            item.active ? (
+                                <li key={item.name}>
+                                    <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                                        onClick={ () => navigate(item.slug)}
+                                    >{item.name}
+                                    </button>
+                                </li>
+                            ) : null
+                        )}
+                        {authStatus && (
+                            <li>
+                                <LogoutBtn/>
+                            </li>
+                        )}
+                    </ul>
+                </nav>
+            </Container>
+        </header>
     )
 }
 
