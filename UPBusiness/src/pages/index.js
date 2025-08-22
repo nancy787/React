f7507ebc9449cf91999185ba2814eb7c8c6ba4c9
nancy787 <nancykumari2630@gmail.com>
2025-08-22 20:18:14 +0530

    Added up business

---
+++ UPBusiness/src/pages/index.js
@@ -0,0 +1,11 @@
+import Card from "../components/Card";
+import Contact from "../components/Contact";
+import Features from "../components/Feature";
+import Home from "../components/Home";
+import Seo from "../components/Seo";
+import Services from "../components/Services";
+import Stats from "../components/Stats";
+import Stats from "../components/Stats";
+import TestimonialCard from "../components/TestimonialCard";
+
+export {Card, Contact, Features, Home, Seo, Services, Stats, TestimonialCard}
